a9b8b9da61b3c120196e77153eecb5b12844fefe
Stephen Wallace <stephen.wallace@slalom.com>
2024-01-17 14:40:46 -0600

    Changed to use pointer

--- problems/rotate_image/rotateimage.go
+++ problems/rotate_image/rotateimage.go
@@ -1,19 +1,27 @@
 /*
 https://leetcode.com/problems/rotate-image/
-
+Note: I rewrote this to use a pointer (maybe badly). The result is that it's ~ms slower, but slightly improves the memory usage :shrug:
 Accepted:
+(no pointer)
 Runtime: 1ms
 Memory: 2.28MB
+
+(using pointer)
+Runtime: 2ms
+Memory: 2.19
+
 */
 package rotateimage
 
 // Processes the rotation by starting with the outer values and iterates across the columns
 // Continues to work inward until the next matrix is < 2x2
 func rotate(matrix [][]int) [][]int {
-	for i := 0; i < (len(matrix) - 1); i++ {
-		opp_val := len(matrix) - i - 1
+	matrix_ptr := &matrix
+	n := len(matrix)
+	for i := 0; i < (n - 1); i++ {
+		opp_val := n - i - 1
 		for j := i; j < opp_val; j++ {
-			rotateSet(matrix, i, j, opp_val)
+			rotateSet(matrix_ptr, i, j, opp_val)
 		}
 	}
 
@@ -27,15 +35,15 @@ func rotate(matrix [][]int) [][]int {
 // i - the starting row index of the sub-matrix
 // j - the starting column index of the sub-matrix
 // opp_val - the ending column/row (square matrix) index
-func rotateSet(matrix [][]int, i int, j int, opp_val int) {
+func rotateSet(matrix *[][]int, i int, j int, opp_val int) {
 	rot_vals := []int{
-		matrix[i][j],
-		matrix[j][opp_val],
-		matrix[opp_val][opp_val-j+i],
-		matrix[opp_val-j+i][i],
+		(*matrix)[i][j],
+		(*matrix)[j][opp_val],
+		(*matrix)[opp_val][opp_val-j+i],
+		(*matrix)[opp_val-j+i][i],
 	}
-	matrix[j][opp_val] = rot_vals[0]
-	matrix[opp_val][opp_val-j+i] = rot_vals[1]
-	matrix[opp_val-j+i][i] = rot_vals[2]
-	matrix[i][j] = rot_vals[3]
+	(*matrix)[j][opp_val] = rot_vals[0]
+	(*matrix)[opp_val][opp_val-j+i] = rot_vals[1]
+	(*matrix)[opp_val-j+i][i] = rot_vals[2]
+	(*matrix)[i][j] = rot_vals[3]
 }
